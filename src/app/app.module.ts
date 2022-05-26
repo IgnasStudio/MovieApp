dfd1dda57ee8dae0bcc401d74a7e551de5ef914b
IgnasBa <eaaigba@students.eaaa.dk>
2022-05-26 15:32:02 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieEffects } from './store/effects/movie.effects';
+import { moviesReducer } from './store/reducers/movies.reducer';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { MoviesFacade } from './store/movies.facade';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieListComponent,
+    MovieDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}),
+    StoreModule.forFeature('movies', moviesReducer),
+    EffectsModule.forRoot([MovieEffects]),
+    StoreDevtoolsModule.instrument({
+      name: 'NgRx Book Store App',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MoviesFacade],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
